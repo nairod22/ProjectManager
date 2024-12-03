d401b04945b9c6c66af74c1baa68c5aba716a1ef
nairod22 <dorian.kury@heig-vd.ch>
2024-12-03 21:19:24 +0100

    multithreading for server

--- src/main/java/ch/heigvd/dai/commands/Server.java
+++ src/main/java/ch/heigvd/dai/commands/Server.java
@@ -4,7 +4,8 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
+
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "server", description = "Start the server part of the network game.")
@@ -18,7 +19,6 @@ public class Server implements Callable<Integer> {
   }
 
   // End of line character
-  //todo : make something for sharing the end of line char ?
   public static String END_OF_LINE = "\n";
 
   @CommandLine.Option(
@@ -29,116 +29,133 @@ public class Server implements Callable<Integer> {
 
   @Override
   public Integer call() {
-    try (ServerSocket serverSocket = new ServerSocket(port)) {
+    try (ServerSocket serverSocket = new ServerSocket(port);
+         ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+
       System.out.println("[SERVER] Listening on port " + port);
 
       while (!serverSocket.isClosed()) {
-        try (Socket socket = serverSocket.accept();
-             Reader reader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
-             BufferedReader in = new BufferedReader(reader);
-             Writer writer =
-                     new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
-             BufferedWriter out = new BufferedWriter(writer)) {
-
-          System.out.println(
-                  "[Server] New client connected from "
-                          + socket.getInetAddress().getHostAddress()
-                          + ":"
-                          + socket.getPort());
-
-          while (!socket.isClosed()) {
-            String clientRequest = in.readLine();
-
-            if (clientRequest == null) {
-              socket.close();
-              continue;
-            }
-
-            String[] clientRequestParts = clientRequest.split(" ", 2);
-
-            Client.Message message = null;
-            String arg = "";
-            try {
-              message = Client.Message.valueOf(clientRequestParts[0]);
-
-              if (clientRequestParts.length > 1) {
-                arg = clientRequestParts[1];
-              }
-            } catch (Exception e) {
-              // Do nothing
-            }
-
-            String response;
-
-            switch (message) {
-              case HELLO -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case PROJL -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case PROJS -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case ADDPRJ -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case DELPR -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case ADDTS -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case DELTS -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case GETTS -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case MODTS -> {
-                System.out.println("[SERVER] receive " + message);
-                response = Message.OKAYY + END_OF_LINE;
-              }
-
-              case null, default -> {
-                System.out.println("[Server] Testing default message");
-                response = "default";
-              }
-            }
-
-            out.write(response);
-            out.flush();
-          }
-
-          System.out.println("[Server] Closing connection");
-        } catch (IOException e) {
-          System.out.println("[Server] IO exception: " + e);
-          return 1;
-        }
+        Socket clientSocket = serverSocket.accept();
+        executor.submit(new ClientHandler(clientSocket));
       }
+
+      return 0;
+
     } catch (IOException e) {
       System.out.println("[Server] IO exception: " + e);
       return 1;
+    } finally {
+      System.out.println("[Server] Closing server");
     }
-
-    return 0;
   }
 
+  static class ClientHandler implements Runnable {
+    private final Socket socket;
+
+    public ClientHandler(Socket socket) {
+      this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+      try (socket;
+           Reader reader = new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8);
+           BufferedReader in = new BufferedReader(reader);
+           Writer writer = new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8);
+           BufferedWriter out = new BufferedWriter(writer)) {
+
+        System.out.println("[Server] New client connected from "
+                + socket.getInetAddress().getHostAddress()
+                + ":"
+                + socket.getPort());
+
+        while (!socket.isClosed()) {
+          String clientRequest = in.readLine();
+
+          if (clientRequest == null) {
+            socket.close();
+            continue;
+          }
+
+          String[] clientRequestParts = clientRequest.split(" ", 2);
+
+          Client.Message message = null;
+          String arg = "";
+          try {
+            message = Client.Message.valueOf(clientRequestParts[0]);
+
+            if (clientRequestParts.length > 1) {
+              arg = clientRequestParts[1];
+            }
+          } catch (Exception e) {
+            // Do nothing
+          }
+
+          String response;
+
+          switch (message) {
+            case HELLO -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case PROJL -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case PROJS -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case ADDPRJ -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case DELPR -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case ADDTS -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case DELTS -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case GETTS -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case MODTS -> {
+              System.out.println("[SERVER] receive " + message);
+              response = Server.Message.OKAYY + END_OF_LINE;
+            }
+
+            case null, default -> {
+              System.out.println("[Server] Testing default message");
+              response = "default";
+            }
+          }
+
+          out.write(response);
+          out.flush();
+        }
+
+        System.out.println("[Server] Closing connection");
+      } catch (IOException e) {
+        System.out.println("[Server] IO exception: " + e);
+      }
+    }
+  }
 }
+
+
 
